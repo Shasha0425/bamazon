095240b9f6253943a9fce0db54b9e018517fd0d5
Shasha <40842477+Shasha0425@users.noreply.github.com>
2018-11-13 22:22:27 -0500

    updates

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,66 +3,68 @@ var inquirer = require("inquirer");
 
 // create the connection information for the sql database
 var connection = mysql.createConnection({
-  host: "localhost",
+    host: "localhost",
 
-  // Your port; if not 3306
-  port: 3306,
+    // Your port; if not 3306
+    port: 3306,
 
-  // Your username
-  user: "root",
+    // Your username
+    user: "root",
 
-  // Your password
-  password: "",
-  database: "bamazon_db"
+    // Your password
+    password: "",
+    database: "bamazon_db"
 });
 
 // connect to the mysql server and sql database
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     // run the start function after the connection is made to prompt the user
     start();
-  });
-  
-  // function which prompts with list of items and askw which item to purchase
-  function start() {
+});
+
+// function which prompts with list of items and askw which item to purchase
+function start() {
     // query the database for all items in the product table
-    connection.query("SELECT * FROM products", function(err, results) {
+    connection.query("SELECT * FROM products", function (err, results) {
         if (err) throw err;
-         console.log(results);  
+        console.table(results);
         // once you have the items, prompts the customer for which item they would like to buy
         inquirer
-        .prompt([
-            {
-            name: "choice",
-            type: "rawlist",
-            choices: function() {
-                var choiceArray = [];
-                for (var i = 0; i < results.length; i++) {
-                choiceArray.push(results[i]);
+            .prompt([
+                {
+                    name: "choice",
+                    type: "input",
+                    message: "Which item(s) would you like to purchase?",
+                    choices: function () {
+                        var choiceArray = [];
+                        for (var i = 0; i < results.length; i++) {
+                            choiceArray.push(results[i].item_id);
+                        }
+                        return choiceArray;
+                    },
+
+
+                },
+
+                {
+                    name: "quantity",
+                    type: "input",
+                    message: "How many would you like to purchase"
                 }
-                return choiceArray;
-                
-            },
-            
-            message: "Which item(s) would you like to purchase?"
-            },
-            {
-            name: "quantity",
-            type: "input",
-            message: "How many would you like to purchase"
+            ])
+            .then(function (answer) {
+                // get the information of the chosen item
+                var chosenItem;
+                for (var i = 0; i < results.length; i++) {
+                    if (results[i].product_name === answer.choice) {
+                        chosenItem = results[i];
+                    }
+                }
             }
-        ])
-        .then(function(answer) {
-            // get the information of the chosen item
-            var chosenItem;
-            for (var i = 0; i < results.length; i++) {
-            if (results[i].product_name === answer.choice) {
-                chosenItem = results[i];
-            }
-         }
-     }
 
-    )}
+            )
+    }
 
-)};
-    
+    )
+};
